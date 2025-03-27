a8c19029208137b54aecdf5e540a9e1ebf788f67
syntrice <syntrice@protonmail.com>
2025-03-27 16:20:51 +0000

    feat: Setup Inter font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "@fontsource/inter"; // Defaults to weight 400
 import App from './App'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
