f3a4d106f4872daaf92cf63283de1b5e5512932b
syntrice <syntrice@protonmail.com>
2025-03-28 15:28:03 +0000

    feat: Start implementing life indicator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import LifeIndicator from "./components/life-indicator/LifeIndicator";
 
 export default function App() {
     return (
         <>
             <Header />
+            <LifeIndicator />
         </>
     )
 }
