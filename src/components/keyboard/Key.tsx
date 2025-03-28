862cc5e943ff74b34204bffc9fdab58555ef6c96
Syntrice <syntrice@protonmail.com>
2025-03-28 17:57:28 +0000

    feat: Create Keyboard component

---
+++ src/components/keyboard/Key.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { KeyState } from "../../models/keyState";
+
+interface KeyProps {
+    keyState: KeyState;
+    children: React.ReactNode;
+    onClick?: () => void;
+}
+
+export default function Key({ keyState, onClick, children }: KeyProps) {
+    const keyColor = () => {
+        switch (keyState) {
+            case KeyState.Correct:
+                return " bg-theme-green";
+            case KeyState.Incorrect:
+                return " bg-theme-red";
+            case KeyState.Unknown:
+                return " bg-theme-amber";
+        }
+    };
+
+    return (
+        <button
+            className={
+                "border-2 rounded-md border-theme-foreground flex w-10 h-10 justify-center items-center text-2xl" +
+                keyColor()
+            }
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,5 +1,14 @@
+import { KeyState } from "../../models/keyState";
+import Key from "./Key";
+
 export default function Keyboard() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
     return (
-        <div className="flex justify-center"><p>Keyboard</p></div>
-    )
+        <div className="flex justify-center flex-wrap gap-3">
+            {alphabet.split("").map((letter, index) => (
+                <Key key={index} keyState={KeyState.Unknown}>{letter}</Key>
+            ))}
+        </div>
+    );
 }
